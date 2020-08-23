06983d9b883217dcf56ff69561de22447dcb4bb0
Luemigues <lulimgues@hotmail.com>
2020-08-22 21:07:05 -0300

    Search funcionando con local storage, back to top y load on scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,25 @@
-// STYLES DINAMICS
 
+// Global vars
+const apiKey = 'api_key=lBi3DfmhAX973lNDIbC2l0hCj4EymuCT'
+const searchInput = document.getElementById('search-input');
 let clickThemes = true;
+let searchedTerms = [];
+
+//Global funcs
+
+function hide(element){
+    element.style.display = "none";
+};
+
+function show(element){
+    element.style.display = "block";
+};
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+};
+
+//THEMES
 
 function showThemes() {
     let themes = document.getElementById('select-theme')
@@ -13,9 +32,21 @@ function showThemes() {
         themes.style.display = 'none';
         clickThemes= true;
     }
-}
+};
 
-let searchInput = document.getElementById('search-input');
+function changeTheme2Night(){
+    let stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = './styles/night-theme/styles-night.css';
+    stylesheet.setAttribute('type',"text/css");
+};
+
+function changeTheme2Day(){
+    let stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = './styles/day-theme/styles-day.css';
+    stylesheet.setAttribute('type',"text/css");
+};
+
+// SUGGESTED SEARCH
 
 function activateSearchButton(){
     
@@ -23,7 +54,7 @@ function activateSearchButton(){
     searchButton.classList.add('colored');
     searchButton.classList.remove('inactive');   
 
-}
+};
 
 function checkEmptyInput() {
     if(searchInput.value.length == 0) {
@@ -44,28 +75,11 @@ function checkEmptyInput() {
         div3.innerText = "";
 
     }
-}
-
-
-function changeTheme2Night(){
-    let stylesheet = document.getElementById('stylesheet');
-    stylesheet.href = './styles/night-theme/styles-night.css';
-    stylesheet.setAttribute('type',"text/css");
-}
-
-function changeTheme2Day(){
-    let stylesheet = document.getElementById('stylesheet');
-    stylesheet.href = './styles/day-theme/styles-day.css';
-    stylesheet.setAttribute('type',"text/css");
-}
-
-// SEARCH 
-
-var apiKey = 'api_key=lBi3DfmhAX973lNDIbC2l0hCj4EymuCT'
-
+};
+ 
 function suggestSearch(){
 
-    let input = document.getElementById('search-input').value;
+    let input = searchInput.value;
     let div1 = document.getElementById('sugg1');
     let div2 = document.getElementById('sugg2');
     let div3 = document.getElementById('sugg3');
@@ -76,7 +90,7 @@ function suggestSearch(){
     .then(res => res.json())
     .then(res => {
 
-        if(input.length > 1){   
+        if(input.length >= 1){   
 
             let suggestions = document.getElementById('search-suggestions');
             suggestions.style.display = 'flex';
@@ -91,85 +105,138 @@ function suggestSearch(){
         }
 
     });
-}
+};
 
-function hide(element){
-    element.style.display = "none";
-}
-
-function show(element){
-    element.style.display = "block";
-}
+//SEARCH RESULTS
 
 function searchFromInput(){
-    let input = document.getElementById('search-input').value
-    showResults(input)
+    let input = searchInput.value;
+    if(input.length >= 1){
+        showResults(input)
+
+        searchedTerms.push(input)
+        localStorage.setItem('search-History', JSON.stringify(searchedTerms));
+
+        document.getElementById('previous-search').innerHTML = '';
+        showSearchHistory();
+    }
+};
+
+function showSearchHistory(){
+    let recentSearch = JSON.parse(localStorage.getItem('search-History'));
+
+    if(recentSearch){
+
+        for(i= (recentSearch.length - 1); i > 0; i--){
+    
+            let container = document.getElementById('previous-search');
+            let searchTerm = document.createElement('div');
+    
+            searchTerm.innerText = recentSearch[i];
+            container.appendChild(searchTerm);
+        }
+    }
+}
+
+
+function showResults(term){
+    
+    let suggestions = document.getElementById('search-suggestions');
+    hide(suggestions)
+    
+    let search = document.getElementById('search-container');
+    let searchSection = document.getElementById('search');
+    let searchTitle = document.getElementById('search-title');
+    
+    searchSection.scrollIntoView(true);
+    
+    window.smoothsc
+    
+    searchTitle.innerText = term;
+    search.textContent = "";
+    searchSection.style.display = 'block';
+    
+    searchInput.value = "";
+    
+    searchGifs(term, 0);
+}
+
+function searchGifs(term, offset){
+    
+    let search = document.getElementById('search-container');
+    
+    fetch('https://api.giphy.com/v1/gifs/search'+ '?' + apiKey + '&q=' + term + '&offset='+ offset)
+    .then(res => res.json())
+    .then(res => showGifsOnGrid(res, search, 5));
 }
 
 function autocompleteAndSearch(id){
     let suggestedTerm = document.getElementById(id).innerText;
-    let input = document.getElementById('search-input').value = suggestedTerm;
-
-    showResults(input)
-}
+    let input = searchInput.value = suggestedTerm;
+    document.getElementById('previous-search').innerText = '';
+    
+    searchedTerms.push(input);
+    localStorage.setItem('search-History', JSON.stringify(searchedTerms));
+    
+    showResults(input);
+    showSearchHistory();
+};
 
 function closeSearch(){
     let search = document.getElementById('search')
     let trending = document.getElementById('trending')
     let suggested = document.getElementById('suggested-today')
-
+    
     hide(search)
     show(trending)
     show(suggested)
-}
+};
 
 // SUGGESTED TODAY
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-}
 
-function showSuggestedToday(){
-
-    let ran1 = getRndInteger(0,6);
-    let ran2 = getRndInteger(7,13);
-    let ran3 = getRndInteger(14,20);
-    let ran4 = getRndInteger(21,27);
-
-    let title1 = document.getElementById('suggested-t1')
-    let gif1 = document.getElementById('suggested-g1')
-
-    let title2 = document.getElementById('suggested-t2')
-    let gif2 = document.getElementById('suggested-g2')
-
-    let title3 = document.getElementById('suggested-t3')
-    let gif3 = document.getElementById('suggested-g3')
-
-    let title4 = document.getElementById('suggested-t4')
-    let gif4 = document.getElementById('suggested-g4')
-
+function suggestCategory(titlePostion, gifPosition, position){
+    let random = [getRndInteger(0,5), getRndInteger(6,11), getRndInteger(12,17), getRndInteger(18,24)]
+    let title = document.getElementById(titlePostion)
+    let gif = document.getElementById(gifPosition)
 
     fetch('https://api.giphy.com/v1/gifs/categories?' + apiKey)
     .then(res => res.json())
     .then(res => {
+
+        let gifData = res.data;
             
-        title1.innerText = '#' + res.data[ran1].gif["tags"][1];
-        gif1.style.backgroundImage = 'url(' + res.data[ran1].gif["images"].downsized_medium["url"] + ')'; 
-        gif1.style.backgroundImage = 'url(' + res.data[ran1].gif["images"].downsized_medium["url"] + ')';
-        
-        title2.innerText = '#' + res.data[ran2].gif["tags"][1];    
-        gif2.style.backgroundImage = 'url(' + res.data[ran2].gif["images"].downsized_medium["url"] + ')'; 
-
-        title3.innerText = '#' + res.data[ran3].gif["tags"][1];
-        gif3.style.backgroundImage = 'url(' + res.data[ran3].gif["images"].downsized_medium["url"] + ')'; 
-
-        title4.innerText = '#' + res.data[ran4].gif["tags"][1]; 
-        gif4.style.backgroundImage = 'url(' + res.data[ran4].gif["images"].downsized_medium["url"] + ')'; 
+        title.innerText = '#' + gifData[random[position]].gif["tags"][1];
+        gif.style.backgroundImage = 'url(' + gifData[random[position]].gif["images"].downsized_medium["url"] + ')'; 
 
     });
+};
+
+function suggestDailyCategories(){
+
+    suggestCategory('suggested-t1','suggested-g1',0)
+    suggestCategory('suggested-t2','suggested-g2',1)
+    suggestCategory('suggested-t3','suggested-g3',2)
+    suggestCategory('suggested-t4','suggested-g4',3)
 }
 
-showSuggestedToday()
+suggestDailyCategories();
+
+function suggestNewCategory(categoryId){
+    if(categoryId == 'suggested-g1'){
+
+        suggestCategory('suggested-t1','suggested-g1',0)
+    } else if(categoryId == 'suggested-g2'){
+
+        suggestCategory('suggested-t2','suggested-g2',1)
+    }else if(categoryId == 'suggested-g3'){
+
+        suggestCategory('suggested-t3','suggested-g3',2)
+    }else if(categoryId == 'suggested-g4'){
+
+        suggestCategory('suggested-t4','suggested-g4',3)
+    }    
+}
 
 function searchCategory(id){
 
@@ -178,28 +245,7 @@ function searchCategory(id){
     showResults(categoryName)
 }
 
-function showResults(term){
 
-    let trending = document.getElementById('trending')
-    let suggested = document.getElementById('suggested-today')
-    let suggestions = document.getElementById('search-suggestions');
-
-    hide(trending)
-    hide(suggested)
-    hide(suggestions)
-
-    let search = document.getElementById('search-container');
-    let searchSection = document.getElementById('search');
-    let searchTitle = document.getElementById('search-title');
-    
-    searchTitle.innerText = term;
-    search.textContent = "";
-    searchSection.style.display = 'block';
-    
-    fetch('https://api.giphy.com/v1/gifs/search'+ '?' + apiKey + '&q=' + term)
-    .then(res => res.json())
-    .then(res => showGifsOnGrid(res, search))
-}
 
 // TRENDING DISPLAY
 
@@ -209,20 +255,23 @@ function showTrending(offset){
 
     fetch('https://api.giphy.com/v1/gifs/trending?' + apiKey + '&limit=25' + '&offset='+ offset)
     .then(res => res.json())
-    .then(res => showGifsOnGrid(res, trendingSection));
+    .then(res => showGifsOnGrid(res, trendingSection, 5));
 }
 
-function showGifsOnGrid(res, append){
+// GIFS IN GRID
+
+function showGifsOnGrid(res, append, rows){
     
     let gifs = res.data;
     let totalgifs = 0;
     let savedGifs = [];
     let filledColumns = 0
+    let gapsToFill = rows * 4;
 
 
     for(let gif of gifs){
        
-        if(totalgifs < 12){
+        if(totalgifs < gapsToFill){
 
         
             let gifWidth = gif.images["downsized"].width;
@@ -271,7 +320,7 @@ function createGif(gif, append, isWide = false){
     div.appendChild(hover);
     append.appendChild(div);
     
-    div.style.backgroundImage = 'url(' + gif.images["downsized"].url + ')'
+    div.style.backgroundImage = 'url(' + gif.images["original"].url + ')'
 
     const regex = ' by';
     let title = gif.title.replace(regex,'');
@@ -286,11 +335,19 @@ function createGif(gif, append, isWide = false){
     }   
 }
 
+showSearchHistory()
+
 let n = 0
 showTrending(n)
 
-// window.onscroll = function(ev) {
-//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-//         showTrending(n+=25)
-//     }
-// };
+window.onscroll = function(ev) {
+    
+    let searchSection = document.getElementById('search').style.display;
+
+        if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && (searchSection == 'block')) {
+            let term = document.getElementById('search-title').innerText;
+            searchGifs(term, n+=25)
+        }else if(((window.innerHeight + window.scrollY) >= document.body.offsetHeight) && (searchSection == 'none')){
+            showTrending(n+=25);
+        }
+    };
